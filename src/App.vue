ce6ad36ae6380f286868e7b2784746a2eb3132ac
tjx12345 <tjx12345@qq.con>
2017-08-22 17:30:29 +0800

    实现轮播图

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
+        <mt-header title="上海9期信息管理系统"></mt-header>
         <router-view></router-view>
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
     </div>
 </template>
 <script>
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,26 +1,38 @@
 <template>
     <div>
-        我是首页
-        <mt-button type="danger" size="large">111</mt-button>
+        <mt-swipe :auto="4000">
+          <mt-swipe-item v-for="(img,index) in imgs" :key="index">
+              <a :href="img.url">
+                <img :src="img.img">
+              </a>
+          </mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 <script>
     export default {
         data(){
             return {
-
+                imgs:[], //轮播图数据
             }
         },created(){
-            this.$ajax.get('getlunbo')
-            .then(res=>{
-                console.log(res);
-            })
-            .catch(err=>{
-                console.log(err);
-            })
+           //发起请求获取轮播图数据
+           this.$ajax.get('getlunbo')
+           .then(res=>{
+                this.imgs = res.data.message;
+           })
+           .catch(err=>{
+            console.log('轮播图获取失败',err);
+           })
         }
     }
 </script>
-<style>
-    
+<style scoped>
+.mint-swipe{
+    height: 200px;
+}
+.mint-swipe img {
+    height: 200px;
+    width: 100%;
+}
 </style>
