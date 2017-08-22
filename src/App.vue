bbfb60594d01af8592b9c7fddfe5ed627b386bf8
tjx12345 <tjx12345@qq.con>
2017-08-22 18:02:38 +0800

    完成底部自定义图标

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,22 @@
         <mt-header title="上海9期信息管理系统"></mt-header>
         <router-view></router-view>
         <nav class="mui-bar mui-bar-tab">
-            <a class="mui-tab-item mui-active" href="#tabbar">
-                <span class="mui-icon mui-icon-home"></span>
+            <router-link :to="{name:'home'}" class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon icon-shouye"></span>
                 <span class="mui-tab-label">首页</span>
-            </a>
-            <a class="mui-tab-item" href="#tabbar-with-chat">
-                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-                <span class="mui-tab-label">消息</span>
-            </a>
-            <a class="mui-tab-item" href="#tabbar-with-contact">
-                <span class="mui-icon mui-icon-contact"></span>
-                <span class="mui-tab-label">通讯录</span>
-            </a>
-            <a class="mui-tab-item" href="#tabbar-with-map">
-                <span class="mui-icon mui-icon-gear"></span>
-                <span class="mui-tab-label">设置</span>
-            </a>
+            </router-link>
+            <router-link :to="{name:'vip'}" class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon icon-huiyuan1"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">会员</span>
+            </router-link>
+            <router-link :to="{name:'shopcart'}"  class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon icon-gouwuche-white-copy"></span>
+                <span class="mui-tab-label">购物车</span>
+            </router-link>
+            <router-link :to="{name:'search'}" class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon icon-work-search"></span>
+                <span class="mui-tab-label">查找</span>
+            </router-link>
         </nav>
     </div>
 </template>
